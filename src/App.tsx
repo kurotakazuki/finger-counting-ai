a62e622833aded8f70215308e0ce2316cc2b16f7
kurotakazuki <kabisakanakabi@gmail.com>
2022-09-19 17:54:58 +0900

    feat: detect finger pose

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,79 @@
+import { useCallback, useEffect, useState, useRef } from "react";
+import { Camera } from "@mediapipe/camera_utils";
+import { Hands, Results } from "@mediapipe/hands";
+import { drawCanvas } from "./utils/drawCanvas";
+import { detectFingerPose } from "./utils/finger";
+
 function App() {
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [localStream, setLocalStream] = useState<MediaStream>();
+
+  const [results, setResults] = useState<Results>();
+
+  const onResults = useCallback((results: Results) => {
+    setResults(results);
+
+    const canvasCtx = canvasRef.current!.getContext("2d")!;
+    drawCanvas(canvasCtx, results);
+  }, []);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        setLocalStream(stream);
+        if (localVideoRef.current) {
+          localVideoRef.current.srcObject = stream;
+          localVideoRef.current.play().catch((e) => console.log(e));
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  // 初期設定
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.7,
+      minTrackingConfidence: 0.5,
+    });
+
+    hands.onResults(onResults);
+
+    if (
+      typeof localVideoRef.current !== "undefined" &&
+      localVideoRef.current !== null
+    ) {
+      const camera = new Camera(localVideoRef.current, {
+        onFrame: async () => {
+          await hands.send({ image: localVideoRef.current! });
+        },
+      });
+      camera.start();
+    }
+  }, [onResults]);
+
   return (
     <div className="App">
-      <video id="input"></video>
-      <canvas id="output" width="600" height="400"></canvas>
+      <video ref={localVideoRef} />
+      <canvas ref={canvasRef} />
+      <div>
+        {results
+          ? results.multiHandLandmarks.map((landmarks) =>
+              detectFingerPose(landmarks)
+            )
+          : null}
+      </div>
     </div>
   );
 }
---
+++ src/utils/finger.ts
@@ -0,0 +1,179 @@
+import { NormalizedLandmarkList, NormalizedLandmark } from "@mediapipe/hands";
+
+// Finger Type
+export const FingerTypes = {
+  REPEAT: "repeat",
+  // Numbers
+  ZERO: "zero",
+  ONE: "one",
+  TWO: "two",
+  THREE: "three",
+  FOUR: "four",
+  FIVE: "five",
+  SIX: "six",
+  SEVEN: "seven",
+  EIGHT: "eight",
+  NINE: "nine",
+} as const;
+export type FingerType = typeof FingerTypes[keyof typeof FingerTypes];
+export const AllFingerType = Object.values(FingerTypes);
+
+function calcAngle(
+  p0: NormalizedLandmark,
+  p1: NormalizedLandmark,
+  p2: NormalizedLandmark
+) {
+  const a1 = p1.x - p0.x;
+  const a2 = p1.y - p0.y;
+  const b1 = p2.x - p1.x;
+  const b2 = p2.y - p1.y;
+  const angle =
+    (Math.acos(
+      (a1 * b1 + a2 * b2) / Math.sqrt((a1 * a1 + a2 * a2) * (b1 * b1 + b2 * b2))
+    ) *
+      180) /
+    Math.PI;
+  return angle;
+}
+
+function fingerAngle(
+  p0: NormalizedLandmark,
+  p1: NormalizedLandmark,
+  p2: NormalizedLandmark,
+  p3: NormalizedLandmark,
+  p4: NormalizedLandmark
+) {
+  let result = 0;
+  result += calcAngle(p0, p1, p2);
+  result += calcAngle(p1, p2, p3);
+  result += calcAngle(p2, p3, p4);
+  return result;
+}
+
+export const detectFingerPose = (
+  landmarks: NormalizedLandmarkList
+): FingerType => {
+  const isThumbOpen =
+    fingerAngle(
+      landmarks[0],
+      landmarks[1],
+      landmarks[2],
+      landmarks[3],
+      landmarks[4]
+    ) < 70;
+  const isIndexFingerOpen =
+    fingerAngle(
+      landmarks[0],
+      landmarks[5],
+      landmarks[6],
+      landmarks[7],
+      landmarks[8]
+    ) < 100;
+  const isMiddleFingerOpen =
+    fingerAngle(
+      landmarks[0],
+      landmarks[9],
+      landmarks[10],
+      landmarks[11],
+      landmarks[12]
+    ) < 100;
+  const isRingFingerOpen =
+    fingerAngle(
+      landmarks[0],
+      landmarks[13],
+      landmarks[14],
+      landmarks[15],
+      landmarks[16]
+    ) < 100;
+  const isPinkieOpen =
+    fingerAngle(
+      landmarks[0],
+      landmarks[17],
+      landmarks[18],
+      landmarks[19],
+      landmarks[20]
+    ) < 100;
+
+  if (
+    !isThumbOpen &&
+    !isIndexFingerOpen &&
+    !isMiddleFingerOpen &&
+    !isRingFingerOpen &&
+    isPinkieOpen
+  ) {
+    return FingerTypes.REPEAT;
+  } else if (
+    isThumbOpen &&
+    isIndexFingerOpen &&
+    isMiddleFingerOpen &&
+    isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.NINE;
+  } else if (
+    isThumbOpen &&
+    isIndexFingerOpen &&
+    isMiddleFingerOpen &&
+    !isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.EIGHT;
+  } else if (
+    isThumbOpen &&
+    isIndexFingerOpen &&
+    !isMiddleFingerOpen &&
+    !isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.SEVEN;
+  } else if (
+    isThumbOpen &&
+    !isIndexFingerOpen &&
+    !isMiddleFingerOpen &&
+    !isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.SIX;
+  } else if (
+    isThumbOpen &&
+    isIndexFingerOpen &&
+    isMiddleFingerOpen &&
+    isRingFingerOpen &&
+    isPinkieOpen
+  ) {
+    return FingerTypes.FIVE;
+  } else if (
+    !isThumbOpen &&
+    isIndexFingerOpen &&
+    isMiddleFingerOpen &&
+    isRingFingerOpen &&
+    isPinkieOpen
+  ) {
+    return FingerTypes.FOUR;
+  } else if (
+    !isThumbOpen &&
+    isIndexFingerOpen &&
+    isMiddleFingerOpen &&
+    isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.THREE;
+  } else if (
+    !isThumbOpen &&
+    isIndexFingerOpen &&
+    isMiddleFingerOpen &&
+    !isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.TWO;
+  } else if (
+    !isThumbOpen &&
+    isIndexFingerOpen &&
+    !isMiddleFingerOpen &&
+    !isRingFingerOpen &&
+    !isPinkieOpen
+  ) {
+    return FingerTypes.ONE;
+  }
+  return FingerTypes.ZERO;
+};
